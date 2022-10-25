490a31ec630b69bca03b3ad448185eaf71eb4acf
Colleen Lemak <clemak11@gmail.com>
2022-10-25 14:18:54 -0700

    up-to-date

---
+++ PythonBasicsFun/sept 9 notes.py
@@ -0,0 +1,30 @@
+# Sept 9 Notes
+# github token: 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
+...
+x = 4/12
+print(x)
+
+x = 4 // 12
+print(x)
+
+x = 4 % 12
+print(x)
+
+print("Please enter the voltage.")
+voltage = float(input())
+
+resistance = float(input("Please enter the resistance. "))
+
+power = voltage ** 2 / resistance
+print("The power is: ", power)
+...
+
+#CONDITIONALS (AKA if statements)
+# if some condition (called boolean condition) is true, then execute some code
+
+x = 6
+if x == 6: # == equality operator (= is assignment operator), need : at the end
+    print("x is 6")
+    # in Python, identation (1 tab or 4 spaces) is used to group code statements into a block
+    # like {} in C/C++
+
